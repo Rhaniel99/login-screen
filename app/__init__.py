42d78dbb5a6de3982469b53638adbebc8135f2da
rhaniel99 <rhanielmonteiro.13@gmail.com>
2023-08-17 23:00:43 -0400

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123*@localhost:3306/flask_login'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'segredo'
+
+login_manager = LoginManager(app)
+db = SQLAlchemy(app)
